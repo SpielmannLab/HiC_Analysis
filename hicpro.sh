5ef15aac29ab2b76ebe2d6f32f0238be9067fbb5
varunkas <varun.sreenivasan@outlook.com>
2023-06-21 12:00:59 +0200

    Update hicpro.sh script to  add line for hicrep

--- hicpro.sh
+++ hicpro.sh
@@ -1,14 +1,13 @@
 #! /bin/bash
 
 #SBATCH --partition=shortterm
-#SBATCH -c 2
-#SBATCH --mem=4GB
-#SBATCH --time=3-0:0:0
+#SBATCH -c 24
+#SBATCH --mem=200GB
 #SBATCH --mail-type=END
-#SBATCH --mail-user=k.schultz@uni-luebeck.de
 #SBATCH --job-name=hicpro
+##SBATCH --tmp=40GB
 
-###later add #SBATCH --tmp=500GB
+conda activate HiC_pro
 
 PATH=$WORK/.omics/anaconda3/bin:$PATH #add the anaconda installation path to the bash path
 source $WORK/.omics/anaconda3/etc/profile.d/conda.sh # some reason conda commands are not added by default
@@ -22,10 +21,18 @@ cat hicpro.yml
 # to 
 # merge_pairs $sample_dir $R1 $R2 ${ldir}
 
-## for parallel mode 
-snakemake -s hicpro.smk hicpro_parallel_step1 --cores $(nproc) --use-conda --conda-prefix $WORK/hic_conda_envs/
-### wait for jobs to finish ?
-#snakemake -s hicpro.smk hicpro_parallel_step2 --cores $(nproc) --use-conda 
 
-snakemake -s hicpro.smk pool --cores $(nproc) --use-conda --conda-prefix $WORK/hic_conda_envs/
+## for parallel mode 
+## snakemake -s hicpro.smk hicpro_parallel_step1 --cores $(nproc) --use-conda --conda-prefix $WORK/hic_conda_envs/
+### wait for jobs to finish ?
+## snakemake -s hicpro.smk hicpro_parallel_step2 --cores $(nproc) --use-conda --conda-prefix $WORK/hic_conda_envs/
+
+# or just run it all.
+# snakemake -s hicpro.smk all --cores $(nproc) --use-conda --conda-prefix $WORK/hic_conda_envs/ #--config SCRATCH=$SCRATCH #Check if $SCRATCH works.
+
+# Do QC
+snakemake -s hicpro.smk hicrep --cores $(nproc) --use-conda --conda-prefix $WORK/hic_conda_envs/ --config SCRATCH=$SCRATCH
+
+# For pooling multiple samples together.
+#snakemake -s hicpro.smk pool --cores $(nproc) --use-conda --conda-prefix $WORK/hic_conda_envs/
 
