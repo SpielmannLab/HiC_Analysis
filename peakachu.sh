f80ddfa37039641f47f70caa51e0bff96406b9f5
varunkas <varun.sreenivasan@outlook.com>
2023-08-30 15:29:04 +0200

    Add sbatch script for peakachu

---
+++ peakachu.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+#SBATCH --partition=shortterm
+#SBATCH -c 3
+#SBATCH --mem=200GB
+#SBATCH --mail-type=END
+#SBATCH --job-name=hicpro
+##SBATCH --tmp=40GB
+
+
+PATH=$WORK/.omics/anaconda3/bin:$PATH #add the anaconda installation path to the bash path
+source $WORK/.omics/anaconda3/etc/profile.d/conda.sh # some reason conda commands are not added by default
+
+cat peakachu.yml
+
+# Run peakachu version 2
+snakemake -s peakachu.smk peakachu_v2 --cores 3 --use-conda --config SCRATCH=$SCRATCH --conda-prefix "$WORK/hic_conda_envs" --conda-frontend "mamba"
